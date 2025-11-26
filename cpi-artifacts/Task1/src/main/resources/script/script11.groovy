a139f37519c6f0389d8cfa728a7d0d2bf233ccde
Rohancherian783 <rohan.cherian@motiveminds.com>
2025-11-26 14:18:32 +0530

    Delete cpi-artifacts/Task1 directory

--- cpi-artifacts/Task1/src/main/resources/script/script11.groovy
+++
@@ -1,7 +0,0 @@
-import com.sap.gateway.ip.core.customdev.util.Message
-
-Message processData(Message message) {
-    // Set attachment filename for this branch
-    message.setHeader("Content-Disposition", "attachment; filename=EmailReport_2.pdf")
-    return message
-}
