4d1f40ef6336dfea66b25f6d950b4cc8cd4d10d2
Rohancherian783 <rohan.cherian@motiveminds.com>
2025-11-27 14:16:37 +0530

    Delete cpi-artifacts/PDFcoveter directory

--- cpi-artifacts/PDFcoveter/Task1/src/main/resources/script/script10.groovy
+++
@@ -1,7 +0,0 @@
-import com.sap.gateway.ip.core.customdev.util.Message
-
-Message processData(Message message) {
-    // Set attachment filename for this branch
-    message.setHeader("Content-Disposition", "attachment; filename=EmailReport_1.pdf")
-    return message
-}
--- cpi-artifacts/PDFcoveter/Task1/src/main/resources/script/script9.groovy
+++
@@ -1,6 +0,0 @@
-import com.sap.gateway.ip.core.customdev.util.Message
-
-Message processData(Message message) {
-    message.setHeader("Content-Disposition", "attachment; filename=EmailReport_2.pdf")
-    return message
-}
